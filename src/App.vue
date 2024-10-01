018847a01f29c352dde352ef6121090391e48478
harumiame <noraja2197@gmail.com>
2024-10-01 10:47:44 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header class="header">
+    <img alt="Vue logo" class="logo" src="./assets/kirbo.png" width="125" height="125" />
+    <div class="wrapper">
+      <HelloWorld msg="Kirbo" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+
+@media (min-width: 1024px) {
+  header {
+    display:flex;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 0 0 0;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,37 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>¿Por qué?</template>
+
+    Porque es importante aprender el uso de frameworks como Vue.js
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>¿Para qué?</template>
+
+    Para poder usarlo en proyectos a futuro
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>¿Por qué un kirby de ícono?</template>
+
+    Porque quería aprender a cambiar el ícono
+  </WelcomeItem>
+</template>
